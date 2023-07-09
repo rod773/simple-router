0fac097888fca2203f9604f6783ae123f6fbfa07
rod773 <104315743+rod773@users.noreply.github.com>
2023-07-09 11:28:37 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+$request = $_SERVER['REQUEST_URI'];
+
+$parts=explode("/",$request);
+
+
+$route = $parts[2];
+
+
+
+switch ($route) {
+   
+    case '' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case 'about' :
+        require __DIR__ . '/views/about.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/views/404.php';
+        break;
+}
